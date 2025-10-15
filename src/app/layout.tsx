868735d94c47ba1cc4d693e0fde25eddb6a3c592
Jeffrey Holmes <coffeman500@gmail.com>
2025-10-14 23:21:17 -0500

    Editor updates and formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "@/styles/globals.css";
 import {ToastContainer} from "react-toastify";
 
 export const metadata: Metadata = {
-  title: "UNT Marketplace",
-  description: "Future home of the UNT Marketplace app",
+    title: "UNT Marketplace",
+    description: "Future home of the UNT Marketplace app",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="antialiased font-display">
-        <div id="appContainer">
-            {children}
-        </div>
-        <ToastContainer />
-      </body>
-    </html>
-  );
+    
+    return (
+        <html lang="en">
+            <body className="antialiased font-display">
+                <div id="appContainer">
+                    {children}
+                </div>
+                <ToastContainer/>
+            </body>
+        </html>
+    );
 }
