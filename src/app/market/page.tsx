7540cc563aa742b107c4b049afb1f54744d4473b
Jeffrey Holmes <coffeman500@gmail.com>
2025-11-01 01:49:59 -0500

    Market listing page
    Initial layout and loading for market listing page.

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,30 +1,20 @@
-﻿"use client"
+﻿"use server"
 
-import Link from "next/link";
+import {Suspense} from "react";
+import {getListings} from "@/actions/listing-actions";
+import ListingsContainer from "@/components/features/ListingsContainer";
+import MarketSuspense from "@/components/suspense/MarketSuspense";
 
-export default function Market() {
+export default async function MarketPage() {
+
+    // Don't await the data fetching function
+    const listingsResponse = getListings();
 
     return (
         <main className="px-20 py-12 flex flex-col gap-6">
-
-            <div>
-                <h1 className="text-4xl">Current Listings</h1>
-            </div>
-
-            <div className="grid grid-cols-3 gap-6 opacity-20">
-
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-                <div className="bg-green rounded-3xl h-[300px]"></div>
-
-            </div>
-
+            <Suspense fallback={<MarketSuspense/>}>
+                <ListingsContainer listingsResponse={listingsResponse} />
+            </Suspense>
         </main>
     );
 }
---
+++ src/components/features/ListingsContainer.tsx
@@ -0,0 +1,40 @@
+﻿"use client"
+
+import React, {use} from "react";
+import {Prisma} from "@/generated/prisma";
+import {ListingReturnType} from "@/models/ListingReturnType";
+
+export default function ListingsContainer({
+    listingsResponse,
+}: {
+    listingsResponse: Promise<ListingReturnType[]>,
+}) {
+    const listings = use(listingsResponse);
+
+    return (
+        <div className="listing-container grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+            {
+                listings.map(listing =>
+                    <div className="listing" key={listing.id}>
+                        <div className="listing-image">
+                            <div className="h-[350px] bg-gray-300 rounded-sm bg-center bg-cover"
+                                 style={{backgroundImage: `url(images/${listing.images[0].url})`}}
+                            ></div>
+                        </div>
+                        <div className="listing-info mt-1">
+                            <p className="font-bold text-lg">
+                                {
+                                    new Intl.NumberFormat('en-US', {
+                                        style: 'currency',
+                                        currency: 'USD',
+                                    }).format(listing.price)
+                                }
+                            </p>
+                            <p>{listing.title}</p>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
