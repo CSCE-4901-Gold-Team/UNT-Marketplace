9af1db80c92446b46807e1a27a69af6fe130d751
Jeffrey Holmes <coffeman500@gmail.com>
2025-11-02 22:42:02 -0600

    Add market wrapper
    Allows state lifting for search/filter.

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,19 +1,17 @@
 ﻿"use server"
 
-import {Suspense} from "react";
 import {getListings} from "@/actions/listing-actions";
-import ListingsContainer from "@/components/features/ListingsContainer";
+import MarketSection from "@/components/features/MarketSection";
+import {Suspense} from "react";
 import MarketSuspense from "@/components/suspense/MarketSuspense";
 
 export default async function MarketPage() {
-
-    // Don't await the data fetching function
     const listingsResponse = getListings();
 
     return (
         <main className="px-20 py-12 flex flex-col gap-6">
             <Suspense fallback={<MarketSuspense/>}>
-                <ListingsContainer listingsResponse={listingsResponse} />
+                <MarketSection listingsResponse={listingsResponse} />
             </Suspense>
         </main>
     );
--- src/components/features/ListingsContainer.tsx
+++ src/components/features/ListingsContainer.tsx
@@ -1,16 +1,14 @@
 ﻿"use client"
 
 import React, {use} from "react";
-import {Prisma} from "@/generated/prisma";
 import {ListingReturnType} from "@/models/ListingReturnType";
 import ListingCard from "@/components/ui/ListingCard";
 
 export default function ListingsContainer({
-    listingsResponse,
+    listings,
 }: {
-    listingsResponse: Promise<ListingReturnType[]>,
+    listings: ListingReturnType[],
 }) {
-    const listings = use(listingsResponse);
 
     return (
         <div className="listing-container items-start grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
