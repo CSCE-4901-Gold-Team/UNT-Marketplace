9d4c4598fe4c27212db33cf8849ca32ce251c4c0
OndreyJ <jondrey2014@gmail.com>
2025-11-20 11:53:56 -0600

    created placeholders for mailjet tests

---
+++ src/lib/test-mailjet-connection.ts
@@ -0,0 +1,9 @@
+// This is a placeholder for the Mailjet connection
+export async function testMailJetConnection() {
+    return { ok: true };
+}
+
+export async function testMailJetEmail() {
+    return { ok: true };
+}
+
