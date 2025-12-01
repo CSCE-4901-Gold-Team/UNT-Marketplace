eb9e1946b801e12dc58508baf7ccfa1d2eeb2bda
Jeffrey Holmes <coffeman500@gmail.com>
2025-11-30 23:44:54 -0600

    Get user status function

--- src/actions/user-actions.ts
+++ src/actions/user-actions.ts
@@ -3,12 +3,17 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { $Enums, PrismaClient } from "@prisma/client";
+import {$Enums, PrismaClient} from "@prisma/client";
 import UserRole = $Enums.UserRole;
+import UserStatusType = $Enums.UserStatusType;
 
 const prisma = new PrismaClient();
 
-export async function getCurrentUserRole() {
+/**
+ * Returns the role of the session's user
+ * @return {Promise<$Enums.UserRole>}
+ */
+export async function getCurrentUserRole(): Promise<$Enums.UserRole> {
     // Validate session
     const session = await auth.api.getSession({
         headers: await headers()
@@ -28,4 +33,60 @@ export async function getCurrentUserRole() {
     });
 
     return user?.role ?? UserRole.STUDENT;
+}
+
+
+/**
+ * Retrieves and returns a User's current UserStatusType
+ *
+ * @param {string} userId The user ID to retrieve UserStatusType for
+ * @return {Promise<{status: boolean, userStatus?: UserStatusType}>}
+ *         Boolean status representing function success, and an optional UserStatusType
+ */
+export async function getUserStatus(userId: string): Promise<{status: boolean, userStatus: UserStatusType | null}> {
+    // Validate session
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+
+    if (!session) {
+        redirect("/sign-in");
+    }
+
+    // Get role of current user and validate
+    const currentUserRole = await getCurrentUserRole();
+    if (!currentUserRole || currentUserRole !== UserRole.ADMIN ) {
+        return {
+            status: false,
+            userStatus: null
+        }
+    }
+
+    // Get current status
+    const userStatus = await prisma.userStatus.findFirst({
+        where: {
+            userId: userId,
+            OR: [
+                { expiresAt: null },
+                {
+                    expiresAt: {
+                        lte: new Date()
+                    }
+                }
+            ]
+        },
+        orderBy: {
+            expiresAt: { sort: "desc", nulls: "first" },
+            createdAt: "desc"
+        },
+        select: {
+            status: true
+        }
+    });
+
+    // Return retrieved status, or active type if no status is found
+    return {
+        status: true,
+        userStatus: userStatus?.status ?? UserStatusType.ACTIVE
+    }
 }
