f1d8373ccffceaec7834132720500cecd1c1ab01
Jeffrey Holmes <coffeman500@gmail.com>
2025-12-04 12:05:53 -0600

    Revert "Design update"

--- src/components/features/MarketSection.tsx
+++ src/components/features/MarketSection.tsx
@@ -9,7 +9,6 @@ import { getListings } from "@/actions/listing-actions";
 import Button from "@/components/ui/Button";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import MarketFilterControls from "@/components/ui/MarketFilterControls";
-import CategoryList from "@/components/ui/CategoryList";
 import { ListingFilters } from "@/types/ListingFilters";
 import { $Enums } from "@prisma/client";
 import UserRole = $Enums.UserRole;
@@ -101,20 +100,11 @@ export default function MarketSection({
 
     return (
         <div id="marketSectionWrapper" className="flex flex-col gap-6">
-            <div>
-                <h1 className="ml-100 mb-20">
-                    Listings
-                </h1>
-                <h2>
-                    Category
-                </h2>
-                <CategoryList />
-            </div>
 
-            <div className="market-controls flex gap-0 justify-between items-center mr-0">
-                <div className="market-search-container translate-x-80 mr-0">
+            <div className="market-controls flex gap-6 justify-between items-center">
+                <div className="market-search-container">
                     <div className="flex">
-                        <TextInput inputClasses="rounded-r-none border-r-0 border border-green text=green placeholder-green-600"
+                        <TextInput inputClasses="rounded-r-none border-r-0"
                             onChange={e => setSearchQuery(e.target.value)}
                             onKeyDown={e => { if (e.key === "Enter") { void searchListings() } }}
                             placeholder="Search..."
@@ -123,7 +113,7 @@ export default function MarketSection({
                     </div>
                 </div>
 
-                <div className="market-filter-container ml-0 translate-x-120">
+                <div className="market-filter-container">
                     <MarketFilterControls
                         filterObject={filterObject}
                         setFilterObjectAction={setFilterObject}
@@ -135,13 +125,13 @@ export default function MarketSection({
 
             {
                 listingsLoading ? <LoadingSpinner /> :
-                    (listings.length === 0 ? <h2 className="text-gray-400 justify-center text-center ml-90 mt-16">No listings found</h2> :
+                    (listings.length === 0 ? <h2 className="text-gray-400 text-center mt-16">No listings found</h2> :
                         <ListingsContainer listings={listings} />)
             }
 
             {
                 !allListingsLoaded && !listingsLoading &&
-                    <div ref={sentinelRef} className="text-center ml-10 pt-12 pb-6">
+                    <div ref={sentinelRef} className="text-center pt-12 pb-6">
                         <LoadingSpinner />
                     </div>
             }
--- src/components/ui/MarketHeader.tsx
+++ src/components/ui/MarketHeader.tsx
@@ -1,7 +1,6 @@
 ﻿"use client"
 
 import React, {useState} from "react";
-import DarkModeToggle from "@/components/ui/DarkModeToggle";
 import MarketSidebar from "@/components/ui/MarketSidebar";
 import {RiMenu3Fill} from "react-icons/ri";
 
@@ -15,18 +14,13 @@ export default function MarketHeader({
     return (
         <div id="marketHeader">
 
-            <div className="flex items-center justify-between">
-                <div className="text-right lg:hidden">
-                    <button
-                        className="p-4 text-4xl"
-                        onClick={() => { setShowSidebar(!showSidebar); }}
-                    >
-                        <RiMenu3Fill />
-                    </button>
-                </div>
-                <div className="pr-4">
-                    <DarkModeToggle />
-                </div>
+            <div className="text-right lg:hidden">
+                <button
+                    className="p-4 text-4xl"
+                    onClick={() => { setShowSidebar(!showSidebar); }}
+                >
+                    <RiMenu3Fill />
+                </button>
             </div>
 
             <MarketSidebar
