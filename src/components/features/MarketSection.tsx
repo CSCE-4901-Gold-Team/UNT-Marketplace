e0458e757e2c807b2973e1b30e570000c17fa40f
Akeel Hanchard <akeelhanchard@gmail.com>
2025-12-07 00:11:19 -0600

    Fix category filer and add an all button

--- src/components/features/MarketSection.tsx
+++ src/components/features/MarketSection.tsx
@@ -115,6 +115,11 @@ export default function MarketSection({
         };
     }, [allListingsLoaded, filterObject, listings, listingsLoading, newPageLoading, pageSize, searchQuery, skipIndex]);
 
+    // Trigger search when category filter changes
+    useEffect(() => {
+        void searchListings();
+    }, [filterObject.categories]);
+
     async function searchListings() {
         if (listingsLoading || newPageLoading) return;
 
