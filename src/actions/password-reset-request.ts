4551c3246c2a6c2fe95bc490211b430015e92210
Akeel Hanchard <akeelhanchard@gmail.com>
2026-02-07 13:43:54 -0600

    Update password-reset-request.ts

--- src/actions/password-reset-request.ts
+++ src/actions/password-reset-request.ts
@@ -30,7 +30,7 @@ export async function passwordResetRequestAction(initialState: FormResponse, for
 
     // Send password reset request
     try {
-        await auth.api.sendPasswordResetEmail({
+        await auth.api.resetPassword({
             body: {
                 email: parsedFormData.data.email,
                 redirectTo: `${process.env.APP_URL}/reset-password`,
