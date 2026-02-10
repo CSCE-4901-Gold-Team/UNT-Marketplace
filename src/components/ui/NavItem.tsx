4e16b07e861d7a076551db2985bd51d56aa81e83
Akeel Hanchard <akeelhanchard@gmail.com>
2026-02-09 23:50:19 -0600

    updating design

--- src/components/ui/NavItem.tsx
+++ src/components/ui/NavItem.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavItem({
     children,
@@ -14,11 +15,14 @@ export default function NavItem({
     setShowSidebarAction: (newVal: boolean) => void;
     className?: string;
 }) {
-
-    const containerClasses = [
-        "flex gap-3 px-3 py-4 rounded-xl dark:hover:bg-gray-700 transition-colors hover:bg-gray-300",
+    const pathname = usePathname();
+    const isActive = pathname === link || (link.includes('?') && pathname === link.split('?')[0]);
+    const containerClasses = [ 
+        isActive ? "bg-green-600 flex gap-3 px-3 py-4 rounded-xl dark:hover:bg-gray-700 transition-colors text-white hover:bg-gray-300 dark:bg-green-700" 
+        : "flex gap-3 px-3 py-4 rounded-xl dark:hover:bg-gray-700 transition-colors hover:bg-gray-300",
         className,
     ].filter(Boolean).join(" ");
+    
 
     return (
         <Link
