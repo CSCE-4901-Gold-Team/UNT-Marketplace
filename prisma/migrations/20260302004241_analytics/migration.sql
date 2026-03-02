f6f927f35f9024f6da2592e4adf60bd98441a84c
Jeffrey Holmes <coffeman500@gmail.com>
2026-03-01 18:43:49 -0600

    Migrations

---
+++ prisma/migrations/20260302004241_analytics/migration.sql
@@ -0,0 +1,35 @@
+-- CreateEnum
+CREATE TYPE "EventType" AS ENUM ('LISTING_IMPRESSION', 'LISTING_VIEW', 'CONTACT_SELLER', 'SAVE_LISTING', 'SHARE_LISTING');
+
+-- DropForeignKey
+ALTER TABLE "report" DROP CONSTRAINT "report_reporterId_fkey";
+
+-- CreateTable
+CREATE TABLE "ListingEvent" (
+    "id" TEXT NOT NULL,
+    "listingId" TEXT NOT NULL,
+    "userId" TEXT,
+    "sessionId" TEXT NOT NULL,
+    "eventType" "EventType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ListingEvent_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "ListingEvent_listingId_idx" ON "ListingEvent"("listingId");
+
+-- CreateIndex
+CREATE INDEX "ListingEvent_userId_idx" ON "ListingEvent"("userId");
+
+-- CreateIndex
+CREATE INDEX "ListingEvent_createdAt_idx" ON "ListingEvent"("createdAt");
+
+-- AddForeignKey
+ALTER TABLE "report" ADD CONSTRAINT "report_reporterId_fkey" FOREIGN KEY ("reporterId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ListingEvent" ADD CONSTRAINT "ListingEvent_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ListingEvent" ADD CONSTRAINT "ListingEvent_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
