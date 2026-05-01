b785ce2c87202d3d9c42c4602a858a95ed4acbe2
Daniel Alan Echevarria <122703214+echedan@users.noreply.github.com>
2026-05-01 18:35:03 -0500

    MessageProfanityFlagStatus conflict resolved

--- src/app/market/listing/[id]/page.tsx
+++ src/app/market/listing/[id]/page.tsx
@@ -8,6 +8,7 @@ import { Suspense } from "react";
 import ListingSuccessToast from "@/components/ui/ListingSuccessToast";
 import { prisma } from "@/lib/prisma";
 import ListingLocationMap from "../../../../components/ui/ListingLocationMap";
+import { MessageProfanityFlagStatus } from "@prisma/client";
 
 export default async function ListingDetail({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
